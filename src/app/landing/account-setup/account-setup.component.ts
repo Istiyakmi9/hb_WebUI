5a18816f8b5f4296da93894c67813855c369c1b6
Marghub <marghub12@gmail.com>
2024-02-12 15:33:53 +0530

    Acoount setup page added

---
+++ src/app/landing/account-setup/account-setup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-setup',
+  templateUrl: './account-setup.component.html',
+  styleUrls: ['./account-setup.component.scss']
+})
+export class AccountSetupComponent {
+
+}
--- src/app/landing/landing-routing.module.ts
+++ src/app/landing/landing-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
-import { Index, JobPost } from 'src/providers/constants';
+import { AccountSetup, Index, JobPost } from 'src/providers/constants';
 import { JobpostComponent } from './jobpost/jobpost.component';
+import { AccountSetupComponent } from './account-setup/account-setup.component';
 
 const routes: Routes = [
   {path: '', component: IndexComponent},
   {path: Index, component: IndexComponent},
-  {path: JobPost, component: JobpostComponent}
-
+  {path: JobPost, component: JobpostComponent},
+  {path: AccountSetup, component: AccountSetupComponent}
 ];
 
 @NgModule({
