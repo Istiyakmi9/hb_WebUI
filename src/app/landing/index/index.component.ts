559b7a734f8576d1f51bb14bc056aafa064b7594
Wasay <mohdwasay90@gmail.com>
2024-05-10 19:39:56 +0530

    ab#139 Job seeker or Job provider option selection page and job apply functionality for the post

--- src/app/landing/index/index.component.ts
+++ src/app/landing/index/index.component.ts
@@ -500,6 +500,22 @@ export class IndexComponent implements OnInit, AfterViewChecked {
 
   }
 
+  addApply(item) {
+    this.isLoading = true;
+    // let value = this.postJobForm.value;
+    this.http.post("userposts/addAppliedPost", item).then((res: ResponseModel) => {
+      if (res.ResponseBody) {
+        Toast("Applied details added");
+        item.IsApplied = true;
+        this.isLoading = false;
+      }
+    }).catch(e => {
+      alert(e.message)
+      this.isLoading = false;
+    })
+
+  }
+
   get f() {
     return this.postJobForm.controls;
   }
